8686f3e7e1536032c831b12a988f4e479f7c1eed
Nodar2004 <53430385+Nodar2004@users.noreply.github.com>
2019-07-30 14:45:43 +0300

    Login system

---
+++ Nodar.py
@@ -0,0 +1,36 @@
+from colorama import * 
+init()
+
+
+
+y = input("do you have a account? select y/n: ")
+
+if y == "y":
+    while True:
+        Email = input("Enter you Email:")
+        Password = input("Enter you password:")
+        ConfrimPassword = input("Confrim your password:")
+        if Password == ConfrimPassword:
+            print("login successful")
+            break
+        if Password != ConfrimPassword:
+            print("Incorrect password")
+            break
+while True:
+    input()
+if y == "n":
+    while True:
+        NewEmail = input("Enter new email:")
+        Newpassword = input("Enter new password:")
+        ConfriPassword = input("Confrim password:")
+        if Newpassword != ConfriPassword:
+            print("Incorrect password, please try again")
+            break
+        if Newpassword == ConfriPassword:
+            print("register success")
+            break
+while True:
+    input()               
+        
+
+
